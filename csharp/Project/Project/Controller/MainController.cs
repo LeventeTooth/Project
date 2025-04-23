b8f4ae6523fb247a823f4d9829926679a794a135
kispruki <kispruki@gmail.com>
2025-04-23 11:42:15 +0200

    ApiCaller 2nd try

--- csharp/Project/Project/Controller/MainController.cs
+++ csharp/Project/Project/Controller/MainController.cs
@@ -2,6 +2,7 @@
 using Project.Model;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,13 +14,22 @@ namespace Project.Controller
         [ObservableProperty]
         private string text;
 
-        private ApiHandler handler;
+        private ApiCaller handler, updater;
 
+        async void run()
+        {
+            //await handler.Post(new List<KeyValuePair<string, string>> { new KeyValuePair<string, string> ("Name", "TestFromCode") });
+            await updater.Update(new List<KeyValuePair<string, string>> { new KeyValuePair<string, string>("Name", "Kurva") });
+            Text =  $"{await handler.Get()}";
+
+        }
 
         public MainController()
         {
 
-            Text = "Hello";
+            handler = new ApiCaller("http://127.0.0.1:8000/api/test");
+            updater = new ApiCaller("http://127.0.0.1:8000/api/test/9");
+            run();
         }
     }
 }
---
+++ csharp/Project/Project/Model/ApiCaller.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model
+{
+    public class ApiCaller
+    {
+        private string url;
+        public ApiCaller(string url)
+        {
+            this.url = url;
+        }
+
+        public async Task<string> Get()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{url}");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> Post(List<KeyValuePair<string, string>> collection)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, $"{url}");
+            
+            var content = new FormUrlEncodedContent(collection);
+            request.Content = content;
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync()   ;
+
+        }
+
+        public async Task<string> Update(List<KeyValuePair<string, string>> collection)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Put, $"{url}");
+            
+            var content = new FormUrlEncodedContent(collection);
+            request.Content = content;
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+
+
+        }
+
+        public async Task<string> Delete()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Delete, $"{url}");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+
+        }
+
+    }
+}
