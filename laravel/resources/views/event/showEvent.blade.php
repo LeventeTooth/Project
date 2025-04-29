2962c777775124106199a54fd607817f7e3cd3b0
Levi <toth.levente.balint@gmail.com>
2025-04-29 21:43:14 +0200

    rounded

--- laravel/resources/views/event/showEvent.blade.php
+++ laravel/resources/views/event/showEvent.blade.php
@@ -18,7 +18,7 @@
             @foreach ($cars as $car)
                 <div
                     class="mt-8 p-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
-                    <img style="width: 100%;" class="rounded-t-lg" src="{{ asset( $car->img) }}" alt="" />
+                    <img style="width: 100%;" class="rounded-lg" src="{{ asset( $car->img) }}" alt="" />
                     <div class="p-5">
                         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ $car->model }}</h5>
                         <p class="mb-3 font-normal text-gray-700 ">{{ $car->power }}</p>
