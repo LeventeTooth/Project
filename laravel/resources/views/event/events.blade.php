2ba05cba7c998e4e7eeeace058d990a1017da984
Pacz <pacz.barnabas@blathy.info>
2025-04-29 21:33:51 +0200

    Dark mode off

--- laravel/resources/views/event/events.blade.php
+++ laravel/resources/views/event/events.blade.php
@@ -6,18 +6,18 @@
 
     <div class="flex flex-wrap justify-center mt-20">
         @foreach ($events as $event)
-            <div class="p-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
                 <a href="#">
                     <img class="rounded-t-lg" src="{{ $event->img }}" alt="" />
                 </a>
                 <div class="p-5">
                     <a href="#">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $event->name }}
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ $event->name }}
                         </h5>
                     </a>
                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $event->date }}</p>
                     <a href="{{ route('events.show', $event->id) }}"
-                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                         Megnézem
                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                             fill="none" viewBox="0 0 14 10">
--- laravel/resources/views/event/showEvent.blade.php
+++ laravel/resources/views/event/showEvent.blade.php
@@ -17,15 +17,15 @@
         <div class="mt-5 pt-5 flex flex-wrap justify-center">
             @foreach ($cars as $car)
                 <div
-                    class="mt-8 p-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                    class="mt-8 p-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
                     <img style="width: 100%;" class="rounded-t-lg" src="{{ asset( $car->img) }}" alt="" />
                     <div class="p-5">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $car->model }}</h5>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $car->power }}</p>
-                        <h3 class="mt-5 mb-6 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $car->price }}
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ $car->model }}</h5>
+                        <p class="mb-3 font-normal text-gray-700 ">{{ $car->power }}</p>
+                        <h3 class="mt-5 mb-6 text-xl font-bold tracking-tight text-gray-900 ">{{ $car->price }}
                             Ft</h3>
                         <a href="{{ route('rents.create', $event, $car) }}"
-                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                             Foglalás
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                 fill="none" viewBox="0 0 14 10">
