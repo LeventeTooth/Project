f923a46942c9d91322185991a1cc1d30017f9633
kispruki <kispruki@gmail.com>
2025-04-29 15:52:27 +0200

    UI update

--- csharp/Project/Project/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Project.AssemblyInfo.cs
+++ csharp/Project/Project/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Project.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+977dcf1583d20603e5c6d4baee0d21ad5fe669f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9c6a669485f48671025a13f3029efe9416e56250")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
