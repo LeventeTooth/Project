532c94620674a14bb1c4a5741652a569dae3ff57
Pacz <pacz.barnabas@blathy.info>
2025-04-29 17:34:40 +0200

    api.php fix

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -13,18 +13,6 @@ Route::get('/user', function (Request $request) {
     return response()->json(User::all());
 });
 
-Route::get('/cars', function () {
-    return response()->json(Car::all());
-});
-
-Route::get('/tracks', function () {
-    return response()->json(Track::all());
-});
-
-Route::get('/groups', function () {
-    return response()->json(Group::all());
-});
-
 
 //Rent api
 Route::resource("/rent", RentController::class);
